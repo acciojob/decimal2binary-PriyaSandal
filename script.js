d1fdb99f53b831398bc791ce146167a95b16c5da
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-07-05 08:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function decimalToBinary(num) {
 }
 
 window.decimalToBinary = decimalToBinary;
+function decimalToBinary(decimal){
+	return decimal.toString(2);
+}
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10));
+console.log(decimalToBinary(33));
